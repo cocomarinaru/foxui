7b6508308b4e2c8dbf8c841a7cd8e6ceece0398c
Ovidiu Sabou <ovidiu.sabou@elan-solutions.eu>
2017-09-18 15:10:38 +0300

    initial commit , from linux,  test on windows

---
+++ Ui.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace foxui
+{
+    class Ui
+    {
+        static void Main(string[] args)
+        {
+            DbConector dbConector= new DbConector();
+            
+            dbConector.connect();
+        }
+    }
+}
